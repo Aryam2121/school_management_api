b134a2884706e8bad0feeec235b4ce838d73d01e
Aryam2121 <aryamangupta2121@gmail.com>
2025-04-22 08:09:45 +0530

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,23 @@
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: 3306,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = pool.promise();
 const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  uri: process.env.DATABASE_URL, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
